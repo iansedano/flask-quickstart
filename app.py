1d4241a3138ff53819ff94841c78ffe009cc1a11
Ian <iansedano@gmail.com>
2021-09-08 08:40:44 +0200

    basic routing

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello Worldouuu!"
+@app.route('/<int:number>/')
+def incrementer(number):
+    return f"{number * number}"
+
+@app.route('/<string:name>/')
+def hello(name):
+    return f"Hello {name}"
     
+@app.route('/person/')
+def hi():
+    return jsonify({'name': 'Jimit', 'address':'India'})
